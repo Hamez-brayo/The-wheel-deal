6e96564ec8ca135cc5ac28d8896bef818cbac363
Hamez-brayo <42073416+Hamez-brayo@users.noreply.github.com>
2022-03-01 15:15:38 +0300

    0.1

---
+++ app/src/main/java/com/example/thewheeldeal/Login.kt
@@ -0,0 +1,39 @@
+package com.example.thewheeldeal
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import androidx.fragment.app.Fragment
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import androidx.navigation.findNavController
+
+class Login : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_login, container, false)
+    }
+
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        view.findViewById<TextView>(R.id.signup).setOnClickListener{
+            view.findNavController().navigate(R.id.action_login_to_sign_up)
+        }
+        view.findViewById<Button>(R.id.button4).setOnClickListener {
+            view.findNavController().navigate(R.id.action_login_to_landing)
+        }
+
+    }
+
+}
+
+
+
+
+
