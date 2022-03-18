aba8f92db14353a889835479052974a10a54a8ba
Hamez-brayo <kariukibrian090@gmail.com>
2022-03-18 12:04:15 +0300

    0.9

---
+++ app/src/main/java/com/example/thewheeldeal/ConvAdapter.kt
@@ -0,0 +1,42 @@
+package com.example.thewheeldeal
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.navigation.findNavController
+import androidx.recyclerview.widget.RecyclerView
+
+class ConvAdapter(private val mList: List<Items>): RecyclerView.Adapter<ConvAdapter.ViewHolder>()  {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ConvAdapter.ViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.landing_page_lists, parent, false)
+
+        return ConvAdapter.ViewHolder(view)
+    }
+    class ViewHolder(ItemView: View) : RecyclerView.ViewHolder(ItemView) {
+        val imageView: ImageView = itemView.findViewById(R.id.imageView15)
+        val textView: TextView = itemView.findViewById(R.id.textView)
+        init {
+            val imageView: ImageView = itemView.findViewById(R.id.imageView15)
+            val textView: TextView = itemView.findViewById(R.id.textView)
+            itemView.setOnClickListener{
+                itemView.findNavController().navigate(R.id.action_landing_to_item_details)
+            }
+        }
+    }
+    override fun onBindViewHolder(holder: ConvAdapter.ViewHolder, position: Int) {
+        val ItemsViewModel = mList[position]
+
+        // sets the image to the imageview from our itemHolder class
+        holder.imageView.setImageResource(ItemsViewModel.image)
+
+        // sets the text to the textview from our itemHolder class
+        holder.textView.text = ItemsViewModel.text
+    }
+
+    override fun getItemCount(): Int {
+        return mList.size
+    }
+}
